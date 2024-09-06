a23cc8fdf38cd1e1f9f7b4b68633dd5a829b6f01
LRMS iMacMX <it@buzola.mx>
2024-09-05 19:30:30 -0600

    feat: JS para fetch campos factura
    Primera version del script para hacer el fetch de campos en factura, unicamente los campos de sales invoice

--- facturacion_mx/facturacion_mx/doctype/factura/factura.js
+++ facturacion_mx/facturacion_mx/doctype/factura/factura.js
@@ -6,3 +6,39 @@
 
 // 	},
 // });
+
+frappe.ui.form.on('factura', {
+    sales_invoice_id: function(frm) {
+    if (frm.doc.sales_invoice_id) {
+        frappe.call({
+            method: 'frappe.client.get',
+            args: {
+                doctype: "Sales Invoice",
+                filters: {
+                    name: frm.doc.sales_invoice_id
+                }
+            },
+            callback: function(r) {
+                if (r.message) {
+                    console.log("------------", r.message);
+                    frm.set_value('cliente', r.message.customer);
+                    frm.set_value('fecha_nota_de_venta', r.message.posting_date);
+                    
+                    frm.clear_table('factura_product_array');
+                    
+                    r.message.items.forEach(function(item) {
+                        var child = frm.add_child('factura_product_array');
+                        
+                        child.producto = item.item_code;
+                        child.descripcion = item.description;
+                        child.cantidad = item.qty;
+                        child.precio = item.rate;
+                    });
+                    
+                    frm.refresh_field('factura_product_array');
+                }
+            }
+        });
+    }
+    }
+});
