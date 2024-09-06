b2e0f306b5846bc5b04c92f15b31efa34dba596a
LRMS iMacMX <it@buzola.mx>
2024-09-05 21:20:59 -0600

    style: format factura.js
    Se formatea unicamente el archivo para que se vea mejor

--- facturacion_mx/facturacion_mx/doctype/factura/factura.js
+++ facturacion_mx/facturacion_mx/doctype/factura/factura.js
@@ -8,76 +8,59 @@
 // });
 
 frappe.ui.form.on('factura', {
-    sales_invoice_id: function(frm) {
-        
+    sales_invoice_id: function (frm) {
+
         frm.clear_table('factura_product_array');
 
-    if (frm.doc.sales_invoice_id) {
-        frappe.call({
-            method: 'frappe.client.get',
-            args: {
-                doctype: "Sales Invoice",
-                filters: {
-                    name: frm.doc.sales_invoice_id
-                }
-            },
-            callback: function(r) {
-                if (r.message) {
-//                    console.log("---SALES INVOICE OBJECT--", r.message);
-                    frm.set_value('cliente', r.message.customer);
-                    frm.set_value('fecha_nota_de_venta', r.message.posting_date);
-                    
-                    frm.clear_table('factura_product_array');
-                    
-                    r.message.items.forEach(function(item) {
-                        var child = frm.add_child('factura_product_array');
-                        
-                        child.producto = item.item_code;
-                        child.descripcion = item.description;
-                        child.cantidad = item.qty;
-                        child.precio = item.rate;     
-                    });
-                    
-                    frm.refresh_field('factura_product_array');
+        if (frm.doc.sales_invoice_id) {
+            frappe.call({
+                method: 'frappe.client.get',
+                args: {
+                    doctype: "Sales Invoice",
+                    filters: {
+                        name: frm.doc.sales_invoice_id
+                    }
+                },
+                callback: function (r) {
+                    if (r.message) {
+                        //                    console.log("---SALES INVOICE OBJECT--", r.message);
+                        frm.set_value('cliente', r.message.customer);
+                        frm.set_value('fecha_nota_de_venta', r.message.posting_date);
 
-                    frappe.call({
-                        method: 'frappe.client.get',
-                        args: { 
-                            doctype: "Customer",
-                            filters: {
-                                name: r.message.customer
+                        frm.clear_table('factura_product_array');
+
+                        r.message.items.forEach(function (item) {
+                            var child = frm.add_child('factura_product_array');
+
+                            child.producto = item.item_code;
+                            child.descripcion = item.description;
+                            child.cantidad = item.qty;
+                            child.precio = item.rate;
+                        });
+
+                        frm.refresh_field('factura_product_array');
+
+                        frappe.call({
+                            method: 'frappe.client.get',
+                            args: {
+                                doctype: "Customer",
+                                filters: {
+                                    name: r.message.customer
+                                }
+                            },
+                            callback: function (s) {
+                                if (r.message) {
+                                    //                                console.log("--CUSTOMER OBJECT---", s.message);
+
+                                    frm.set_value('tax_id', s.message.tax_id);
+                                    frm.set_value('tax_category', s.message.tax_category);
+
+                                }
                             }
-                        },
-                        callback: function(s) {
-                            if (r.message) {
-//                                console.log("--CUSTOMER OBJECT---", s.message);
-                                
-                                frm.set_value('tax_id', s.message.tax_id);
-                                frm.set_value('tax_category', s.message.tax_category);
-                                
-                            }
-                            
-                        }
-                        
-                    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                        });
+                    }
                 }
-            }
-        });
-    }
+            });
+        }
     }
 });
