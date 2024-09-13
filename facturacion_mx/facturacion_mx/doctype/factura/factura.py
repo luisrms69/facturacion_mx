dcf259b45afe2e8eae5c68b8d20f07c0f1bcb438
Luis Montanaro Developer env <admin@appcity.mx>
2024-09-13 09:52:39 -0600

    clean: elimnacion de codigo comentado no necesario
    Se elimina codigo no necesario en factura.py

--- facturacion_mx/facturacion_mx/doctype/factura/factura.py
+++ facturacion_mx/facturacion_mx/doctype/factura/factura.py
@@ -6,38 +6,6 @@ from frappe import _
 from frappe.model.document import Document
 import requests #Se utiliza para hacer el http request
 
-#import inspect
-
-# def whatis(message, backend=True, frontend=True):
-# 	"""
-# 	This function is using in debugging, and shows an object's value, type, and call stack.
-# 	"""
-# 	inspected_stack = inspect.stack()
-
-# 	direct_caller = inspected_stack[1]
-# 	direct_caller_linenum = direct_caller[2]
-
-# 	parent_caller = inspected_stack[2]
-# 	parent_caller_function = parent_caller[3]
-# 	parent_caller_path = parent_caller[1]
-# 	parent_caller_line = parent_caller[2]
-
-# 	message_type = str(type(message)).replace('<', '').replace('>', '')
-# 	msg = f"---> DEBUG (frappe.whatis)\n"
-# 	msg += f"* Initiated on Line: {direct_caller_linenum}"
-# 	msg += f"\n  * Value: {message}\n  * Type: {message_type}"
-# 	msg += f"\n  * Caller: {parent_caller_function}"
-# 	msg += f"\n  * Caller Path: {parent_caller_path}\n  * Caller Line: {parent_caller_line}\n"
-
-# 	if backend:
-# 		print(msg)
-# 	if frontend:
-# 		msg = msg.replace('\n', '<br>')
-# 		frappe.msgprint(msg)
-
-
-
-
 
 class Factura(Document):
     
@@ -110,29 +78,12 @@ class Factura(Document):
           sales_invoice_id = frappe.db.get_value('Factura', current_document, 'sales_invoice_id' )
           invoice_data = frappe.get_doc('Sales Invoice', sales_invoice_id )
           items_info = Factura.get_items_info(invoice_data)
-
           cliente = Factura.get_cliente(invoice_data)
-#          customer_data = frappe.get_doc('Customer', cliente )
-#          tax_id = customer_data.tax_id
           tax_id = Factura.get_tax_id(cliente)
-#          regimen_fiscal = customer_data.tax_category[:3]
           regimen_fiscal = Factura.get_regimen_fiscal(cliente)
-          # filters = [
-          #      ["Dynamic Link", "link_doctype", "=", "Customer"],
-          #      ["Dynamic Link", "link_name", "=", cliente],
-          #      ["Address", "is_primary_address", "=", 1]
-          # ]
-          # company_address = frappe.get_all("Address", filters=filters)
-          # datos_direccion = frappe.db.get_value('Address', company_address,['pincode', 'email_id'], as_dict=1)
           datos_direccion = Factura.get_datos_direccion_facturacion(cliente)
-          # filters = [
-          #      ["Payment Entry Reference", "reference_doctype", "=", "Sales Invoice"],
-          #      ["Payment Entry Reference", "reference_name", "=", sales_invoice_id]
-          # ]
-          # pay_entry = frappe.get_all("Payment Entry", filters=filters)
-          # metodo_de_pago = frappe.db.get_value("Payment Entry", pay_entry, "mode_of_payment")[:2]
           metodo_de_pago = Factura.get_metodo_de_pago(sales_invoice_id)
-          
+
           facturapi_endpoint = "https://www.facturapi.io/v2/invoices"
           api_token = "sk_test_rBbmpjK2Mq0oE8GXvx2Qe6E3blga45PnR9YkZOAJLQ"
           headers = {"Authorization": f"Bearer {api_token}"}
@@ -151,10 +102,7 @@ class Factura(Document):
                "items": items_info               
           }
           response = requests.post(facturapi_endpoint,json=data,headers=headers)
-
-#          whatis(response)
-
-
+          
                     
      def on_submit(self):
          self.create_cfdi()
