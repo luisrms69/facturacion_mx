33ea24ce1becf15e865fa1c736f6894973c4c387
Luis Montanaro Developer env <admin@appcity.mx>
2024-09-13 12:20:42 -0600

    feat: add facturacion_mx settings page
    Se crea el doctype para los settings, es de tipo single. se añaden campos basicos (py, js, json ,test)

---
+++ facturacion_mx/facturacion_mx/doctype/facturacion_mx_settings/facturacion_mx_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Consultoria en Negocios y Aplicaciones and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Facturacion MX Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
