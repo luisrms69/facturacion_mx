93af33258d0372b03595738cdde67135a72e2587
Luis Montanaro Developer env <admin@appcity.mx>
2024-09-19 22:58:43 -0600

    feat: logica revision status cx
    Se copia la logica para actualizacion completa del estado al revisar el estado de la cancelacion, no es muy kosher ya que hay mucho codigo duplicado. la siguietne tarea es refactorizar el codigo en cx factura para que ocupe el de la API

--- facturacion_mx/facturacion_mx/doctype/cancelacion_factura/api.py
+++ facturacion_mx/facturacion_mx/doctype/cancelacion_factura/api.py
@@ -39,6 +39,15 @@ def actualizar_cancelacion_respuesta_pac(pac_response):  #refactor: esto se debe
                 msg=f"El estatus reportado por el PAC en la solicitud es: {message_status} y el estatus de cancelación es: {message_cancellation_status}",
                 title='La solicitud de cancelación fue exitosa.',
                 indicator='green')
+        
+        return status
+        
+def actualizar_status_factura(doc, status):
+    #   doc = frappe.get_doc("Cancelacion Factura", factura_a_cancelar) #refactor: duplicado abajo se pued calcular antes y pasar a los metodos
+      doc.db_set({
+            'status' : status
+      })
+      
     #     self.db_set({
     #     'status' : status
     # })
@@ -53,10 +62,44 @@ def actualizar_cancelacion_respuesta_pac(pac_response):  #refactor: esto se debe
     #     'mensaje_de_error' : pac_response['message']
     # })
 
+
+#fix:refacto este metodo se trajo de cx factura, esta duplicado
+def anade_response_record(doc,pac_response):	#refactor: esta lista debera estar en una variable para hacer un foreach o algo por el estilo
+    # if CancelacionFactura.determine_resultado(pac_response) == 1:
+    # doc = frappe.get_doc("Cancelacion Factura", factura_a_cancelar)
+    doc.append("respuestas", 
+                {
+                    'response_id': pac_response['id'],
+                    'status_response' : pac_response['status'],
+                    'cancellation_status' : pac_response['cancellation_status'],
+                    'verification_url' : pac_response['verification_url'],
+                    'uuid' : pac_response['uuid'],
+                    'fecha_de_creacion' : pac_response['created_at'],
+                    'folio' : pac_response['folio_number'],
+                    'serie_de_facturacion': pac_response['series'],
+                    'monto_total': pac_response['total'],
+                    'forma_de_pago': pac_response['payment_form'],
+                    'id_del_cliente': pac_response['customer']['id'],
+                    'nombre_del_cliente': pac_response['customer']['legal_name'],
+                    'rfc': pac_response['customer']['tax_id'],
+                    'signature': pac_response['stamp']['signature'],
+                    'fecha_de_sellado': pac_response['stamp']['date'],
+                    'numero_de_certificado_sat': pac_response['stamp']['sat_cert_number'],
+                    'firma_sat': pac_response['stamp']['signature']
+                    })
+    doc.save()
+
+
+
+
         
 @frappe.whitelist()
-def status_check_cx_factura(factura_a_revisar):
-        factura_object = get_factura_object(factura_a_revisar)
-        actualizar_cancelacion_respuesta_pac(factura_object)
+def status_check_cx_factura(id_factura, factura_a_cancelar):
+        factura_object = get_factura_object(id_factura)
+        status = actualizar_cancelacion_respuesta_pac(factura_object)
+        doc = frappe.get_doc("Cancelacion Factura", factura_a_cancelar)
+        anade_response_record(doc, factura_object)
+        actualizar_status_factura(doc,status)
+
 
         # frappe.msgprint(str(factura_object))
