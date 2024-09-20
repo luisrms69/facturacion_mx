61147dab6d6cb1e556c4776a1ad264ac8cfd99b1
Luis Montanaro Developer env <admin@appcity.mx>
2024-09-19 21:47:15 -0600

    feat: separa metodo obtener status factura
    Se separa el metodo obtener status, asi podra ser utilizado por otros metodos que lo necesite

--- facturacion_mx/facturacion_mx/doctype/cancelacion_factura/api.py
+++ facturacion_mx/facturacion_mx/doctype/cancelacion_factura/api.py
@@ -9,8 +9,8 @@ from frappe.utils.password import get_decrypted_password #se importa para poder 
 
 
 # Metodo que se llaman en factura.js para obtener alguna forma de pago, en caso de que exista
-@frappe.whitelist()
-def status_check_cx_factura(factura_a_revisar):
+# @frappe.whitelist()
+def get_factura_object(factura_a_revisar):
         api_token = get_decrypted_password('Facturacion MX Settings', 'Facturacion MX Settings', "live_secret_key")
         headers ={ "Authorization": f"Bearer {api_token}"}
         factura_endpoint = frappe.db.get_single_value('Facturacion MX Settings', 'endpoint_obtener_facturas')
@@ -19,5 +19,12 @@ def status_check_cx_factura(factura_a_revisar):
         response = requests.get(final_url, headers=headers)
         
         data_response =response.json()
+
+        return data_response
         
-        frappe.msgprint(str(data_response))
+        
+@frappe.whitelist()
+def status_check_cx_factura(factura_a_revisar):
+        factura_object = get_factura_object(factura_a_revisar)
+
+        frappe.msgprint(str(factura_object))
