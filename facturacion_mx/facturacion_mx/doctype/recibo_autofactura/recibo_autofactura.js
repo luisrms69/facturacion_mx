056de62528e3fee5426a56ac9590839a272cbbcc
Luis Montanaro Developer env <admin@appcity.mx>
2024-09-20 22:30:04 -0600

    feat: Funcionalidad JS Recibo Autofactura
    Funcionalidad JS para visualizar los datos del recibo y que se actualicen en pantalla

--- facturacion_mx/facturacion_mx/doctype/recibo_autofactura/recibo_autofactura.js
+++ facturacion_mx/facturacion_mx/doctype/recibo_autofactura/recibo_autofactura.js
@@ -6,3 +6,65 @@
 
 // 	},
 // });
+
+
+frappe.ui.form.on('Recibo Autofactura', {
+    sales_invoice_id: function (frm) {
+        if (frm.doc.sales_invoice_id) {
+            frappe.call({
+                method: 'frappe.client.get',
+                args: {
+                    doctype: "Sales Invoice",
+                    filters: {
+                        name: frm.doc.sales_invoice_id
+                    }
+                },
+                callback: function (r) {
+                    if (r.message) {
+                        // frm.set_value('fecha_nota_de_venta', r.message.posting_date);
+                        frm.clear_table('recibo_product_array')
+
+                        // refactor: esto es igual a factura.js
+                        r.message.items.forEach(function (item) {
+                            var child = frm.add_child('recibo_product_array');
+                            child.producto = item.item_code;
+                            child.descripcion = item.description;
+                            child.cantidad = item.qty;
+                            child.precio = item.rate;
+                        });
+                        frm.refresh_field('recibo_product_array');
+                        frappe.call({
+                            method: 'frappe.client.get',
+                            args: {
+                                doctype: "Customer",
+                                filters: {
+                                    name: r.message.customer
+                                }
+                            },
+                            callback: function (s) {
+                                if (s.message) {
+                                    frm.set_value('rfc', s.message.tax_id);
+                                    // frm.set_value('tax_category', s.message.tax_category);
+                                }
+                            }
+                        });
+                    }
+                }
+            }),
+            frappe.call({
+                method: 'facturacion_mx.facturacion_mx.doctype.factura.api.get_forma_de_pago',
+                args: {
+                    sales_invoice_id: frm.doc.sales_invoice_id
+                },
+                callback: function (t) {
+                    if (t.message) {
+                    frm.set_value('forma_de_pago_registrada', t.message);
+                    } else {
+                    frm.set_value('forma_de_pago_registrada', "No hay referencia de forma de pago")
+                }
+            }
+            });
+        }
+    }
+});
+
