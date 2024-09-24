aaf819d901b0639f6c26ece39ec50961711ccaaa
Luis Montanaro Developer env <admin@appcity.mx>
2024-09-24 13:43:08 -0600

    feat: descarga archivos factura
    Queda funcionando el codigo para descargar archivos, de aqui en adelante son mejoras

--- facturacion_mx/facturacion_mx/api.py
+++ facturacion_mx/facturacion_mx/api.py
@@ -9,6 +9,8 @@ from frappe.utils.password import get_decrypted_password #se importa para poder 
 from frappe.utils import validate_email_address
 from frappe.utils.response import *
 import re #fix: Se incluye por que venía en el metodo para obtener el nombre del archivo en descarga factura, no estoy seguro si se usa
+import json # lo cargo para utilizar json.loads
+
 
 def get_filename_from_cd(cd):
         if not cd:
@@ -17,6 +19,37 @@ def get_filename_from_cd(cd):
         if len(fname) == 0:
                 return None
         return fname[0]
+
+def presenta_ultimos_caracteres(str_var, caracteres):
+        length = len(str_var)
+        str_final = str_var[length - caracteres:]
+
+        return str_final
+
+def save_to_factura(filename_dir):
+        api_secret='b93d45547b0fa48'
+        api_key= '542c9e12488dca5'
+        url = 'http://127.0.0.1:8000/api/method/upload_file'
+        headers = {"Authorization": f"token {api_key}:{api_secret}",
+                   'Accept': "application/json"
+                #    'Content-Type': "pdf"
+                   }
+        files ={
+                'file': open(filename_dir, 'rb'),
+        }
+        data = {
+                'is_private': 1,
+                'doctype': "Factura",
+                'docname': "FACT-24-09-22-0272"
+        }
+        response = requests.post(url=url, data=data, headers=headers, files=files)
+        # response.dict = json.loads(response.text)
+
+        # file_name = response.dict['message']['name']
+        file_name = json.loads(response.text)['message']['name']
+        frappe.errprint(response.__dict__)
+
+        return file_name
 
 # Metodo que se llaman en factura.js para descargar la factura
 @frappe.whitelist()
@@ -34,14 +67,14 @@ def descarga_factura(current_document,format):
 
         path="/home/erpnext/files/" #Se requiere crear por separado manualmente
         filename = get_filename_from_cd(response.headers.get('content-disposition'))[1:-1]
-        filename_dir = path + filename
+        filename_short = presenta_ultimos_caracteres(filename,15)
+        filename_dir = path + filename_short
         with open(filename_dir, 'wb') as file:
-                file.write(response.content)
+                file.write(response.content)  #refactor: ver opcion señalada abajo para no ocupar tanto ram
 
-
-        # download(filename)
+        save_to_factura(filename_dir)
         
-
+        # refactor option: con esto podemos disminuir el uso del ram
         # with open("/home/erpnext/frappe-bench/apps/facturacion_mx/archivo.xml", 'wb') as local_file:
         #       for chunk in response.iter_content(chunk_size=128):
         #              local_file.write(chunk)
@@ -49,24 +82,24 @@ def descarga_factura(current_document,format):
 
 
 
-@frappe.whitelist()
-def test_crud(filename_dir):
-        # frappe.msgprint("Testing CRUD") 
-        api_secret='b93d45547b0fa48'
-        api_key= '542c9e12488dca5'
-        url = 'http://127.0.0.1:8000/api/method/upload_file'
-        headers = {"Authorization": f"token {api_key}:{api_secret}",
-                   'Accept': "application/json"
-                #    'Content-Type': "pdf"
-                   }
-        files ={
-                'file': open(filename_dir, 'rb'),
-        }
-        response = requests.post(url=url, headers=headers, files=files)
-        # response.dict = json.loads(response.text)
+# @frappe.whitelist()
+# def test_crud(filename_dir):
+#         # frappe.msgprint("Testing CRUD") 
+#         api_secret='b93d45547b0fa48'
+#         api_key= '542c9e12488dca5'
+#         url = 'http://127.0.0.1:8000/api/method/upload_file'
+#         headers = {"Authorization": f"token {api_key}:{api_secret}",
+#                    'Accept': "application/json"
+#                 #    'Content-Type': "pdf"
+#                    }
+#         files ={
+#                 'file': open(filename_dir, 'rb'),
+#         }
+#         response = requests.post(url=url, headers=headers, files=files)
+#         # response.dict = json.loads(response.text)
 
-        # file_name = response.dict['message']['name']
-        file_name = json.loads(response.text)['message']['name']
-        # frappe.msgprint(file_name)
+#         # file_name = response.dict['message']['name']
+#         file_name = json.loads(response.text)['message']['name']
+#         # frappe.msgprint(file_name)
 
-        return file_name
+#         return file_name
