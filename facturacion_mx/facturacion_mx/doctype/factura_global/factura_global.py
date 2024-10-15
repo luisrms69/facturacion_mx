0e0c53421d1f23320e75636b02e85b5ac2c3d86c
Luis Montanaro Developer env <admin@appcity.mx>
2024-10-15 13:34:42 -0600

    clean: Factura Global
    Elimino codigo basura en factura global

--- facturacion_mx/facturacion_mx/doctype/factura_global/factura_global.py
+++ facturacion_mx/facturacion_mx/doctype/factura_global/factura_global.py
@@ -9,42 +9,10 @@ from facturacion_mx.facturacion_mx.api import *
 # import requests  # Se utiliza para hacer el http request
 # from frappe.utils.password import get_decrypted_password #se importa para poder acceder al password
 # from frappe.utils import validate_email_address
-# from .api import *
 
 
 class FacturaGlobal(Document):
-
-# #refactor: este codigo ya no tiene sentido asi, ya verificamos el status en el metodo anterior
-# #toma la respuesta y las llaves deseadas y la prepara para escritura en el documento
-#     def check_pac_response(data_response,keys):
-#         pac_response = {'status' : "Facturado" }
-#         for key in keys:
-#             if key in data_response.keys():
-#                 pac_response[key] = data_response[key]
-#             else:
-#                 pac_response = { 'status' : "Rechazada" }
-
-#         return pac_response
-
-
-# # refactor: deberia poder tener la info de los campos a actualizar en una lista como la funcion de check_pac
-# # Añade informacion en caso de exito al documento
-#     def update_pac_response(self,pac_response):
-#         self.db_set({
-#             'id_pac': pac_response['id'],
-#             'uuid' : pac_response['uuid'],
-#             'url_de_verificación' : pac_response['verification_url'],
-#             'serie_de_la_factura' : pac_response['series'],
-#             'folio_de_factura' : pac_response['folio_number'],
-#             'fecha_timbrado' : pac_response['created_at'],  #refactor: no se trata de la fecha de timbrado es la fehca de emision
-#             'status' : pac_response['status'],
-#             'monto_total' : pac_response['total']
-#         })
-
-# #Actualiza el sales invoice como facturado Normal
-#     def update_sales_invoice_status(sales_invoice_id):
-#         frappe.set_value('Sales Invoice', sales_invoice_id, 'custom_status_facturacion', "Factura Normal")
-    
+   
 #Metodo para solicitar la creacion de una factura global
 	def create_cfdi_global(self):
 		current_document = self.get_title()
@@ -94,18 +62,11 @@ class FacturaGlobal(Document):
         }
 
 
-		frappe.msgprint(str(forma_de_pago))
-
-
-		# Tareeas a realizar con la respuesta
 		#Cambia el estado de las notaas de venta a enviadas a PAC
 		cambia_status_invoice_list_global(invoice_list,"Enviado a PAC")
 
 		response = requests.post(facturapi_endpoint, json=data, headers=headers)
-		# 		self.anadir_response_record(response)
-
 		data_response =response.json()
-		# frappe.msgprint(str(data_response))
 
 		table_respuestas = "respuestas_del_pac"
 		anade_response_record(table_respuestas, self,data_response)
@@ -128,48 +89,6 @@ class FacturaGlobal(Document):
 		despliega_aviso(title=aviso_titulo, msg=aviso_message, color=aviso_color)
 
 
-		# status = self.actualizar_cancelacion_respuesta_pac(response)
-		# actualizar_status_cx_factura(self, status)
-		# self.anadir_response_record(response)
-
-		# if status == "Cancelacion Exitosa" :
-		# 	actualizar_status_factura_invoice(self.name)
-
-
-# # La respuesta se almacena, se convierte a JSON y se verifica si fue exitosa o rechazada
-# #se avisa al usuario el resultado y se escribe en el documento dependiendo del resultado
-# # Si fue exitosa se marca en sales invoice com facturado
-#         response = requests.post(
-#             facturapi_endpoint, json=data, headers=headers)
-        
-#         data_response =response.json()
-
-
-#         if check_pac_response_success(response) == 1:
-#             factura_pac_keys = ['id','uuid','verification_url','series','folio_number', 'created_at', 'total']
-#             pac_response = Factura.check_pac_response(data_response,factura_pac_keys)
-
-#             if pac_response['status'] == "Facturado":
-#                 self.update_pac_response(pac_response)
-#                 Factura.update_sales_invoice_status(sales_invoice_id)
-#                 frappe.msgprint(
-#                     msg="La solicitud de facturación ha sido exittosa, puedes consultar los detalles de la confirmación proporcionados por el PAC en la parte inferior de este documento",
-#                     title='Solicitud exitosa!!',
-#                     indicator='green'             
-#                 )
-#             else:
-#                 self.db_set['status'] = "Rechazada" # refactor: no creo que sea necesario este else
-#         else:
-#             self.db_set({
-#                 'status' : "Rechazada",
-#                 'response_rechazada' : str(data_response)  #refactor: dejar en una sola seccion exito y fracaso
-#                          })
-#             frappe.msgprint(
-#                 msg=str(data_response),
-#                 title='La solicitud de facturacion no fue exitosa',
-#                 indicator='red'
-#             )
-
 #Metodo que se corre para validar si los campos son correctos        
 	def validate(self):
 		validate_cliente_publico_en_general()
@@ -177,7 +96,6 @@ class FacturaGlobal(Document):
 		validate_orden_fechas(self.fecha_inicial, self.fecha_final, msg_orden_fechas)
 		msg_invoice_empty = "No existen notas de venta pendientes de facturación para el periodo seleccionado"
 		validate_not_empty(self.notas_de_venta, msg_invoice_empty)
-
 
 
 #Metodo que se corre al enviar (submit) solicitar creacion de la factura
