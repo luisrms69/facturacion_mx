b5226528c579a5851ec56409870fa2aecb5b2c6d
Luis Montanaro Developer env <admin@appcity.mx>
2024-10-15 18:40:36 -0600

    refactor: hooks.py
    Solamente se tiene opcion ahora para exortar custom fields , se queda comentada

--- facturacion_mx/hooks.py
+++ facturacion_mx/hooks.py
@@ -243,5 +243,5 @@ app_license = "mit"
 # }
 
 fixtures = [ "Custom Field", "Uso CFDI", "Mode of Payment", "Forma de Pago", "Tax Category" , "Motivo de Cancelacion", "Periodicidad SAT", "Meses y Bimestres SAT", "UOM","UOM Conversion Factor" ]
-
+# fixtures = [ "Custom Field"]
 # fixtures = [ "UOM","UOM Conversion Factor" ]
