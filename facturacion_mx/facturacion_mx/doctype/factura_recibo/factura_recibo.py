f3348d4887f39fdf8e13652ffd6fcd6ca771509a
Luis R Montanaro S <it@buzola.mx>
2024-12-12 19:10:05 -0600

    clean: removed FacturaRecibo doctype
    Opte por usar la liga que genera el sistema, lo mas practico

--- facturacion_mx/facturacion_mx/doctype/factura_recibo/factura_recibo.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Consultoria en Negocios y Aplicaciones and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class FacturaRecibo(Document):
-	pass
--- facturacion_mx/facturacion_mx/doctype/factura_recibo/test_factura_recibo.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Consultoria en Negocios y Aplicaciones and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestFacturaRecibo(FrappeTestCase):
-	pass
