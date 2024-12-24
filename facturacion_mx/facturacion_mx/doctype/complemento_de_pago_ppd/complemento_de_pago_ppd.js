3e33374eeb14e81b5260e8075fa4bc91b7d86934
LRMS MacBook PRO <it@buzola.mx>
2024-12-24 12:19:05 -0600

    feat: complemento de pago 0.00
    Primera version esqueleto de complemento de pago
    js, se copia factura.js y se empiezan a hacer modificaiones
    json, empieza a tomar forma, estoy tomando como referencia factura
    py, se copia factura y de ahi se trabajara

--- facturacion_mx/facturacion_mx/doctype/complemento_de_pago_ppd/complemento_de_pago_ppd.js
+++ facturacion_mx/facturacion_mx/doctype/complemento_de_pago_ppd/complemento_de_pago_ppd.js
@@ -6,3 +6,277 @@
 
 // 	},
 // });
+
+
+//GET FECHA DE VENTA, PRODUCTOS, RFC Y REGIMEN FISCAL
+// Se hace una llamada al metod client.get utilizando como entrada el sales_invoice_id capturado
+// El callback se utiliza para definir la fecha de venta, los productos, el RFC y el regimen fiscal
+// Para los productos se tiene que hacer un loop
+// Para el RFC y el regimen fiscal se tiene que utilizar un segundo call a client get utilizando el valor de customer obtenido
+// El tercer call es a un script elaborado en esta app para encontrar, si existiera, alguna forma de pago utilizada para pagar la venta
+// El valor del tercer call se utiliza como referencia al usuario, pero este tiene que seleccionar manualmente
+frappe.ui.form.on("Complemento de Pago PPD", {
+    entrada_de_pago_id: function (frm) {
+        if (frm.doc.entrada_de_pago_id) {
+            frappe.call({
+                method: 'frappe.client.get',
+                args: {
+                    doctype: "Payment Entry",
+                    filters: {
+                        name: frm.doc.entrada_de_pago_id
+                    }
+                },
+                callback: function (r) {
+                    console.log("#######r message#########")
+                    console.log(r.message);
+                    if (r.message) {
+                        frm.set_value('fecha_de_pago', r.message.posting_date);
+                        frm.set_value('forma_de_pago', r.message.mode_of_payment);
+                        // frm.clear_table('documentos_relacionados_con_el_pago')
+                        console.log(r.message)
+
+                        // r.message.items.forEach(function (item) {
+                        //     var child = frm.add_child('factura_product_array');
+                        //     child.producto = item.item_code;
+                        //     child.descripcion = item.description;
+                        //     child.cantidad = item.qty;
+                        //     child.precio = item.rate;
+                        };
+                        // frm.refresh_field('factura_product_array');
+                        // frappe.call({
+                        //     method: 'frappe.client.get',
+                        //     args: {
+                        //         doctype: "Customer",
+                        //         filters: {
+                        //             name: r.message.party_name
+                        //         }
+                        //     },
+                        //     callback: function (s) {
+                        //         if (s.message) {
+                        //             frm.set_value('tax_id', s.message.tax_id);
+                        //             frm.set_value('tax_category', s.message.tax_category);
+                        //         }
+                        //     }
+                        // });
+                    }
+                })
+            }
+            // }),
+                // frappe.call({
+                //     method: 'facturacion_mx.facturacion_mx.api.get_forma_de_pago',
+                //     args: {
+                //         sales_invoice_id: frm.doc.sales_invoice_id
+                //     },
+                //     callback: function (t) {
+                //         if (t.message) {
+                //             // console.log("#######server script message#########");
+                //             // console.log(t.message);
+                //             frm.set_value('referencia_de_pago', t.message);
+                //         } else {
+                //             frm.set_value('referencia_de_pago', "No hay referencia de forma de pago")
+                //         }
+                //     }
+                // });
+        }
+    }
+// });
+)
+
+
+//refactor:deberia poder llamar a la funcion con el dotted path
+//refactor: debe tenerse el codigo hardocded en alguna variable
+//refactor: las siguientes funciones dependen que la factura tenga estado facturado, creo que pueden meterse en uno solo
+
+// Codigo que genera boton en la Factura para hacer el envio por correo y llama al método PY de envio
+// frappe.ui.form.on('Factura', {
+//     refresh: function (frm) {
+//         if (frm.doc.status == "Facturado") {
+//             frm.add_custom_button(__('Enviar por Correo'), function () {
+//                 let d = new frappe.ui.Dialog({
+//                     title: 'Selecciona el correo electronico al que quieres enviar la factura',
+//                     fields: [
+//                         {
+//                             label: 'Correo Electronioco',
+//                             fieldname: 'email_id',
+//                             fieldtype: 'Data',
+//                             default: frm.doc.email_id
+//                         }
+//                     ],
+//                     size: 'small', // small, large, extra-large 
+//                     primary_action_label: 'Submit',
+//                     primary_action: function () {
+//                         var data = d.get_values();
+//                         frappe.call({
+//                             method: 'facturacion_mx.facturacion_mx.api.envia_factura_por_email',
+//                             args: {
+//                                 current_document: frm.doc.name,
+//                                 email_id: data.email_id
+//                             },
+//                             callback: function (r) {
+//                                 if (r.message) {
+//                                     console.log("#######server script message#########");
+//                                     console.log(r.message);
+//                                 }
+//                                 d.hide();
+//                             }
+//                         });
+//                     }
+//                 });
+
+//                 d.show();
+//             })
+//         }
+//     }
+// });
+
+// // Codigo que genera boton en la Factura para desccargar el archivo deseados y llama al método PY de envio
+// frappe.ui.form.on('Factura', {
+//     refresh: function (frm) {
+//         if (frm.doc.status == "Facturado") {
+//             frm.add_custom_button(__('Descargar'), function () {
+//                 let d = new frappe.ui.Dialog({
+//                     title: 'Selecciona el formato en que quieres descargar la factura',
+//                     fields: [
+//                         {
+//                             label: 'Formato Deseado',
+//                             fieldname: 'format',
+//                             fieldtype: 'Select',
+//                             default: 'zip',
+//                             options: "xml\npdf\nzip"
+//                         }
+//                     ],
+//                     size: 'small', // small, large, extra-large 
+//                     primary_action_label: 'Submit',
+//                     primary_action: function () {
+//                         var data = d.get_values();
+//                         frappe.call({
+//                             method: 'facturacion_mx.facturacion_mx.api.descarga_factura',
+//                             args: {
+//                                 document_name: frm.doc.name,
+//                                 // current_document: frm.doc.id_pac,
+//                                 format: data.format
+//                             },
+//                             callback: function (r) {
+//                                 if (r.message) {
+//                                     console.log("#######server script message#########");
+//                                     console.log(r.message);
+//                                 }
+//                                 d.hide();
+//                             }
+//                         });
+//                     }
+//                 });
+
+//                 d.show();
+//             })
+//         }
+//     }
+// });
+
+
+
+// // Codigo que genera boton en la Factura para cancelar y llama al método PY de envio
+// // Se deben tener que automatizar para utilizar el doctype Motivo de Cancelacion
+// //refactor: estaba originalmente en un loop pero al seleccionar cualquier opcion se tomaba el valor de "i", que era el final del loop
+
+// frappe.call({
+//     method: "frappe.client.get_list",
+//     args: {
+//         doctype: "Motivo de Cancelacion",
+//         fields: ["motivo_de_cancelación", "descripcion"],
+//     },
+//     async: false,
+//     callback(r) {
+//         if (r.message) {
+//             motivos_de_cancelacion = r.message
+//             // console.log(motivos_de_cancelacion)
+//             // console.log(motivos_de_cancelacion[1])
+//             // console.log(motivos_de_cancelacion[1].descripcion)
+//             // console.log(motivos_de_cancelacion[1].motivo_de_cancelación)
+//             // console.log(r.message)
+//         }
+//     },
+// });
+
+// // motivos_cancelacion =["01 Comprobante emitido con errores con relación", "02 Comprobante emitido con errores sin relación", "03 No se llevó a cabo la operación", "04 Operación nominativa relacionada en la factura global"] //refactor: tomar de la variable global
+// frappe.ui.form.on('Factura', {
+//     refresh: function (frm) {
+//         if (frm.doc.status == "Facturado") {  //refactor: tomar de la variable global
+//             frm.add_custom_button(__(motivos_de_cancelacion[0].descripcion), function () {  // Debo obtener el valor de la cancelacion automaticamente
+//                 frappe.call({
+//                     method: 'facturacion_mx.facturacion_mx.api.cancela_factura',
+//                     args: {
+//                         doc: frm.doc.name,
+//                         motivo: motivos_de_cancelacion[0].motivo_de_cancelación
+//                     },
+//                     callback: function (r) {
+//                         if (r.message) {
+//                             console.log("#######server script message#########");
+//                             console.log(r.message);
+//                         }
+//                     }
+//                 });
+//             }, __("Cancelaciones")
+//             );
+//             frm.add_custom_button(__(motivos_de_cancelacion[1].descripcion), function () {  // Debo obtener el valor de la cancelacion automaticamente
+//                 frappe.call({
+//                     method: 'facturacion_mx.facturacion_mx.api.cancela_factura',
+//                     args: {
+//                         doc: frm.doc.name,
+//                         motivo: motivos_de_cancelacion[1].motivo_de_cancelación
+//                     },
+//                     callback: function (r) {
+//                         if (r.message) {
+//                             console.log("#######server script message#########");
+//                             console.log(r.message);
+//                         }
+//                     }
+//                 });
+//             }, __("Cancelaciones")
+//             );
+//             frm.add_custom_button(__(motivos_de_cancelacion[2].descripcion), function () {  // Debo obtener el valor de la cancelacion automaticamente
+//                 frappe.call({
+//                     method: 'facturacion_mx.facturacion_mx.api.cancela_factura',
+//                     args: {
+//                         doc: frm.doc.name,
+//                         motivo: motivos_de_cancelacion[2].motivo_de_cancelación
+//                     },
+//                     callback: function (r) {
+//                         if (r.message) {
+//                             console.log("#######server script message#########");
+//                             console.log(r.message);
+//                         }
+//                     }
+//                 });
+//             }, __("Cancelaciones")
+//             );
+
+//             frm.add_custom_button(__(motivos_de_cancelacion[3].descripcion), function () {  // Debo obtener el valor de la cancelacion automaticamente
+//                 frappe.call({
+//                     method: 'facturacion_mx.facturacion_mx.api.cancela_factura',
+//                     args: {
+//                         doc: frm.doc.name,
+//                         motivo: motivos_de_cancelacion[3].motivo_de_cancelación
+//                     },
+//                     callback: function (r) {
+//                         if (r.message) {
+//                             console.log("#######server script message#########");
+//                             console.log(r.message);
+//                         }
+//                     }
+//                 });
+//             }, __("Cancelaciones")
+//             );
+//         }
+//     }
+// });
+
+
+// forma_pago_ppd_ingreso = "99" //refactor: tomar del doctype ambas
+// name_metodo_ppd = "METODOPAGO-PPD-Pago en parcialidades o diferido"
+
+// frappe.ui.form.on("Factura", "metodo_pago_sat", function (frm) {
+//     if (frm.doc.metodo_pago_sat == name_metodo_ppd) {
+//         frm.set_value("foma_de_pago_sat", forma_pago_ppd_ingreso)
+//     };
+// })
--- facturacion_mx/facturacion_mx/doctype/complemento_de_pago_ppd/complemento_de_pago_ppd.py
+++ facturacion_mx/facturacion_mx/doctype/complemento_de_pago_ppd/complemento_de_pago_ppd.py
@@ -2,8 +2,98 @@
 # For license information, please see license.txt
 
 # import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
-
+import requests  # Se utiliza para hacer el http request
+from frappe.utils.password import get_decrypted_password #se importa para poder acceder al password
+from facturacion_mx.facturacion_mx.api import *
 
 class ComplementodePagoPPD(Document):
-	pass
+    def create_complemento(self):
+# Variables documento actual
+        current_document = self.get_title()
+        payment_entry_id = frappe.db.get_value(
+            'Complemento de Pago PPD', current_document, 'payment_entry_id')
+        payment_data = frappe.get_doc('Payment Entry', payment_entry_id)
+        cliente = get_cliente(payment_data)
+        datos_direccion = get_datos_direccion_facturacion(cliente)
+        tax_id = get_tax_id(cliente)
+        email_id = datos_direccion.email_id
+
+
+# Pendiente configuración o automatización
+        type = "P"
+        payment_related_ids =[]
+        payment_method = frappe.db.get_value('Metodo de Pago', frappe.db.get_value('Factura', current_document, 'metodo_pago_sat'), 'metodo_pago')
+
+# Pendiente configuración de estos campos, NO SE VAN A OCUPAR, SE DEJA EL PLACER
+        currency = "MXN"
+        exchange = 1  # ESTO DEBERA CONFIGURARSE DE OTRA MANERA
+        conditions = ""
+        related_documents = []
+        export = "01"
+        complements = []
+        status = "pending"
+        date = "" # ESTE CAMPO NO LO VOY A CONFIGURAR, EL DEFAULT ES NOW
+        address = {}
+        external_id = ""
+        idempotency_key = ""
+        namespaces = []
+        pdf_options = {}
+
+#Despues se arma el http request. endpoint, headers y data. Los valores de headers y endpoint se toman de settings
+#Los valores de data se arman en este metodo, hacen llamadas a los metodos de la clase creada (Factura)
+        facturapi_endpoint = frappe.db.get_single_value('Facturacion MX Settings','endpoint_crear_facturas')
+        api_token = get_api_token_live()
+        # api_token = get_decrypted_password('Facturacion MX Settings','Facturacion MX Settings',"live_secret_key")
+        headers = {"Authorization": f"Bearer {api_token}"}
+        data = {
+            "type": type,
+            "customer": {
+                "legal_name": cliente,
+                "tax_id": tax_id,
+                "tax_system": get_regimen_fiscal(cliente),
+                "email": email_id,
+                "address": {
+                    "zip": datos_direccion.pincode
+                },
+            },
+            "payment_form": frappe.db.get_value('Factura', current_document, 'foma_de_pago_sat'),
+            "payment_method": payment_method,
+            
+            # "currency": currency, VIENE POR DEFAULT
+            # "exchange": exchange, VIENE POR DEFAULT
+            # "conditions": conditions, NO VIENE PORQUE NO SE ENVIA, NO LO TENGO INCLUIDO EN LA DEFINICION DE INVOICE OBJECT
+            "related_documents": related_documents,
+            "export": export,
+            "complements": get_complements_info(payment_data),
+            # "status": status,
+            "external_id": external_id,
+            # "folio_number": folio_number,
+            # "series": series,
+            # "pdf_custom_section": pdf_custom_section,
+            # "addenda": addenda,
+            "namespaces": namespaces,
+            # "pdf_options": pdf_options,
+            "idempotency_key" : idempotency_key,
+            # "payment_related_ids": payment_related_ids, SOLO LO ACEPTA CUANDO SE TRATA DE PPD
+        }
+
+        response = requests.post(
+            facturapi_endpoint, json=data, headers=headers)
+
+        status_doc , status_sales_invoice = respuesta_pac_factura(self, response)
+
+        actualizar_status_doc(self,status_doc)
+        # actualizar_status_sales_invoice(self.sales_invoice_id,status_sales_invoice)
+ 
+#Metodo que se corre para validar si los campos son correctos
+# refactor: si es lo mismo en receipts, crear funcion que agrupe todo   
+def validate(self):
+        validate_data_invoice(self)
+
+#Metodo que se corre al enviar (submit) solicitar creacion de la factura
+def on_submit(self):
+        # actualizar_status_sales_invoice(self.sales_invoice_id,"Enviado a PAC")  #fix:debera tomarse de la variable global, mismo caso que Recibo Autofactura
+        self.create_complento()
