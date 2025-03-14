d85a1376f61411fa8cafb9284e127e66577b7bb3
Aspire V5 <it@buzola.mx>
2025-03-13 19:47:01 -0600

    feat: crea doctype complemento cliente
    Se crea el doctype complemento cliente, donde se guaradara el texto del complemento especial de los clientes

---
+++ facturacion_mx/facturacion_mx/doctype/complemento_cliente/test_complemento_cliente.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Consultoria en Negocios y Aplicaciones and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestComplementoCliente(FrappeTestCase):
+	pass
